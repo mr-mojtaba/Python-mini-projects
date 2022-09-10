ac13d9871d3c5197d0839ea008769db995a2e5c5
Mr.MOJTABA <97731139+mr-mojtaba@users.noreply.github.com>
2022-09-10 15:10:36 +0430

    add project 08

---
+++ S03.P03.Q01.Create a dictionary and get its key and value from the user.py
@@ -0,0 +1,15 @@
+# سید مجتبی موسوی / تکالیف مبحث دیکشنری و مجموعه / سوال 1 
+
+word = {}
+Meanings = []
+
+w = input("Enter the word: ")
+
+Meanings.append(input("Enter the meaning 1: "))
+Meanings.append(input("Enter the meaning 2: "))
+Meanings.append(input("Enter the meaning 3: "))
+Meanings.append(input("Enter the meaning 4: "))
+
+word[w] = Meanings
+
+print(word)
