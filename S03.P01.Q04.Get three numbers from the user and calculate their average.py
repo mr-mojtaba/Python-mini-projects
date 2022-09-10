dbda52ceb84f9a9cfef5c041a6d7b5d0cd9d08e9
Mr.MOJTABA <97731139+mr-mojtaba@users.noreply.github.com>
2022-09-10 15:07:09 +0430

    add project 04

---
+++ S03.P01.Q04.Get three numbers from the user and calculate their average.py
@@ -0,0 +1,10 @@
+# سید مجتبی موسوی
+# تکالیف مبحث اعداد
+# سوال 4
+
+x1 = int(input("Enter first number: "))
+x2 = int(input("Enter second number: "))
+x3 = int(input("Enter third number: "))
+r1 = x1 + x2 + x3
+r2 = r1 / 3
+print("Average = ", r2)
