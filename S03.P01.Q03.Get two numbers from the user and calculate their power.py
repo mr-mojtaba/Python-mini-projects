f81250ee1635deda6e5c559c98694ea4cde2ba0b
Mr.MOJTABA <97731139+mr-mojtaba@users.noreply.github.com>
2022-09-10 15:06:46 +0430

    add project 03

---
+++ S03.P01.Q03.Get two numbers from the user and calculate their power.py
@@ -0,0 +1,8 @@
+# سید مجتبی موسوی
+# تکالیف مبحث اعداد
+# سوال 3
+
+x1 = int(input("Enter first number: "))
+x2 = int(input("Enter second number: "))
+r = x1 ** x2
+print("===> ", x1, "**", x2, "=", r)
