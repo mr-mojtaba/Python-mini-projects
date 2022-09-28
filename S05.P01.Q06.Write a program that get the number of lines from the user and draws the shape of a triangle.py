829be4081e1e55458cff3a30daa2122ca33ae1fb
Mr.MOJTABA <97731139+mr-mojtaba@users.noreply.github.com>
2022-09-28 14:18:33 +0330

    Delete S05.P01.Q06.Write a program that get the number of lines from the user and draws the shape of a triangle.py
    untidy

--- S05.P01.Q06.Write a program that get the number of lines from the user and draws the shape of a triangle.py
+++
@@ -1,7 +0,0 @@
-# سید مجتبی موسوی / تکالیف مبحث حلقه و اعداد تصادفی / سوال 6
-
-n = int(input("Enter the number of Rows: "))
-
-for i in range(1, n+1):
-    print(" " * (n - i), end="")
-    print("*" * i)
