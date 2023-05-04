1ee4c5be0d84dd41b24cbab84ce55fd16e5bc53e
S.MOJTABA MOUSAVI <97731139+mr-mojtaba@users.noreply.github.com>
2023-05-04 13:07:44 +0330

    Delete S06.P02.Q06.Write a program using lambda to find out if a given string starts with a given character..py

--- S06.P02.Q06.Write a program using lambda to find out if a given string starts with a given character..py
+++
@@ -1,9 +0,0 @@
-# سید مجتبی موسوی / تکالیف مبحث لامبدا / سوال 6
-#  با استفاده از لامبدا برنامه ای بنویسید تا بفهمید که آیا یک رشته داده شده با یک کاراکتر مشخص شروع می شود یا خیر
-
-e_word = input("Type a Word: ")
-e_ch = input("Type a Char: ")
-
-check_ch = lambda ch: True if e_word.startswith(e_ch) else False
-
-print(check_ch(e_ch))
